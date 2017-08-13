8f0de5e5506152ed179819ab37d027b7161f17c5
Jun-seon <hjs0579@ajou.ac.kr>
2017-08-13 14:56:17 +0900

    movieclassifier

---
+++ make_pkl.py
@@ -0,0 +1,113 @@
+import numpy as np
+import re
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+stop = stopwords.words('english')
+porter = PorterStemmer()
+
+def tokenizer(text):
+    text = re.sub('<[^>]*>', '', text)
+    emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)', text.lower())
+    text = re.sub('[\W]+', ' ', text.lower()) + ' '.join(emoticons).replace('-', '')
+    tokenized = [w for w in text.split() if w not in stop]
+    return tokenized
+
+def stream_docs(path):
+    with open(path, 'r') as csv:
+        next(csv) # skip header
+        for line in csv:
+            text, label = line[:-3], int(line[-2])
+            yield text, label
+
+next(stream_docs(path='./movie_data.csv'))
+
+def get_minibatch(doc_stream, size):
+    docs, y = [], []
+    try:
+        for _ in range(size):
+            text, label = next(doc_stream)
+            docs.append(text)
+            y.append(label)
+    except StopIteration:
+        return None, None
+    return docs, y
+
+from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.linear_model import SGDClassifier
+
+vect = HashingVectorizer(decode_error='ignore',
+                         n_features=2**21,
+                         preprocessor=None,
+                         tokenizer=tokenizer)
+
+clf = SGDClassifier(loss='log', random_state=1, n_iter=1)
+doc_stream = stream_docs(path='./movie_data.csv')
+
+import pyprind
+pbar = pyprind.ProgBar(45)
+
+classes = np.array([0, 1])
+for _ in range(45):
+    X_train, y_train = get_minibatch(doc_stream, size=1000)
+    if not X_train:
+        break
+    X_train = vect.transform(X_train)
+    clf.partial_fit(X_train, y_train, classes=classes)
+    pbar.update()
+
+X_test, y_test = get_minibatch(doc_stream, size=5000)
+X_test = vect.transform(X_test)
+print('Accuracy: %.3f' % clf.score(X_test, y_test))
+
+clf = clf.partial_fit(X_test, y_test)
+
+import pickle
+import os
+
+dest = os.path.join('./', 'pkl_objects')
+if not os.path.exists(dest):
+    os.makedirs(dest)
+
+pickle.dump(stop, open(os.path.join(dest, 'stopwords.pkl'), 'wb'), protocol=4)
+pickle.dump(clf, open(os.path.join(dest, 'classifier.pkl'), 'wb'), protocol=4)
+
+from sklearn.feature_extraction.text import HashingVectorizer
+import re
+import os
+import pickle
+
+cur_dir = os.path.dirname(__file__)
+stop = pickle.load(open(
+                os.path.join(cur_dir,
+                'pkl_objects',
+                'stopwords.pkl'), 'rb'))
+
+def tokenizer(text):
+    text = re.sub('<[^>]*>', '', text)
+    emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)',
+                           text.lower())
+    text = re.sub('[\W]+', ' ', text.lower()) \
+                   + ' '.join(emoticons).replace('-', '')
+    tokenized = [w for w in text.split() if w not in stop]
+    return tokenized
+
+vect = HashingVectorizer(decode_error='ignore',
+                         n_features=2**21,
+                         preprocessor=None,
+                         tokenizer=tokenizer)
+
+import pickle
+import re
+import os
+from vectorizer import vect
+
+clf = pickle.load(open(os.path.join('pkl_objects', 'classifier.pkl'), 'rb'))
+
+import numpy as np
+label = {0:'negative', 1:'positive'}
+
+example = ['I love this movie']
+X = vect.transform(example)
+print('Prediction: %s\nProbability: %.2f%%' %\
+      (label[clf.predict(X)[0]], clf.predict_proba(X).max()*100))
